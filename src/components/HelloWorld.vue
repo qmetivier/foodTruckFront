235c03f1af69270af4fe5e897c34ab05735caa14
qmetivier <quentin.metivier@epsi.fr>
2020-10-07 16:12:56 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,134 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <label for="calcPosi">Donner sa position :
+        <button id="calcPosi" v-on:click="getRandXY">Donner</button>
+      </label>
+    </div>
+    <div></div>
+    <div>
+      <label for="xPosi">X:
+        <input type="number" id="xPosi" v-model="X" disabled>
+      </label>
+      <label for="yPosi">Y:
+        <input type="number" id="yPosi" v-model="Y" disabled>
+      </label>
+    </div>
+    <br />
+    <div>
+      <label for="findTrucks">Trouver les 10 Food Trucks les plus proches :
+        <button id="findTrucks" v-on:click="getFoodTrucks">Trouver</button>
+      </label>
+    </div>
+    <br />
+    <div class="tableau">
+      <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Distance</th>
+        <th>Coordonées</th>
+        <th>Lien Google Map</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(truck, index) in trucks" v-bind:key="truck.name + '_' + index">
+        <td>{{truck.name}}</td>
+        <td>{{truck.distance}}Km</td>
+        <td>X: {{truck.coordinates.X}} <br> Y: {{truck.coordinates.Y}}</td>
+        <td><a :href="generateLink(truck.coordinates.X, truck.coordinates.Y)">
+          <img src="../assets/Google_Maps_icon.svg" alt="">
+        </a></td>
+      </tr>
+      </tbody>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      urlWebSocket: 'ws://localhost:5050',
+      client: WebSocket.prototype,
+      X: 0,
+      Y: 0,
+      trucks: [],
+    };
+  },
+  methods: {
+    getRandXY() {
+      this.X = this.getRandomFloat(-89, 89);
+      this.Y = this.getRandomFloat(-179, 179);
+    },
+    getRandomFloat(min, max) {
+      return Math.random() * (max - min) + min;
+    },
+    generateLink(X, Y) {
+      return `https://www.google.com/maps/dir//${X},${Y}`;
+    },
+    webSocketConnect() {
+      this.client = new WebSocket('ws://localhost:5050');
+      this.client.onmessage = function (event) {
+        this.trucks = event.data;
+      };
+      this.client.onclose = function () {
+      };
+      this.client.onopen = function () {
+        this.client.send(JSON.stringify({ X: 0.0, Y: 0.0 }));
+        this.client.close();
+      };
+    },
+    webSocketConnectFake() {
+      this.trucks = [
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        { name: 'truck', distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+      ];
+    },
+    getFoodTrucks() {
+      this.webSocketConnectFake();
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.tableau{
+  width: 500px;
+  margin: 0 auto;
+
+  table {
+    border: medium solid #6495ed;
+    border-collapse: collapse;
+    width: 50%;
+  }
+  th {
+    font-family: monospace;
+    border: thin solid #6495ed;
+    width: 50%;
+    padding: 5px;
+    background-color: #D0E3FA;
+  }
+  td {
+    font-family: sans-serif;
+    border: thin solid #6495ed;
+    width: 50%;
+    padding: 5px;
+    text-align: center;
+    background-color: #ffffff;
+  }
+  caption {
+    font-family: sans-serif;
+  }
+
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,66 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import WS from 'jest-websocket-mock';
+import HelloWorld from '../../src/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  beforeEach(async () => {
+    window.server = new WS("ws://localhost:1234");
+    window.client = new WebSocket("ws://localhost:1234");
+    await window.server.connected;
+  });
+
+  afterEach(() => {
+    WS.clean();
+  });
+  it('connects to the websocket server', async () => {
+    const { server, client } = window;
+    await server.connected;
+    client.send('hello');
+    await expect(server).toReceiveMessage('hello');
+    expect(server).toHaveReceivedMessages(['hello']);
+  });
+
+  it('test the websocket is good', async () => {
+    HelloWorld.methods.getRandXY();
+
+    const { server, client } = window;
+    await server.connected;
+    HelloWorld.data().client = client;
+    HelloWorld.data.client.send(JSON.stringify({ X: 1.000102, Y: 178.05421245 }));
+    await expect(server).toReceiveMessage(JSON.stringify({ X: 1.000102, Y: 178.05421245 }));
+    expect(server).toHaveReceivedMessages([JSON.stringify({ X: 1.000102, Y: 178.05421245 })]);
+    server.send(JSON.stringify(
+      {
+        coo: { X: 0, Y: 0 },
+        data: [
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        ],
+      },
+    ));
+    expect(HelloWorld.data().trucks).toEqual(JSON.stringify(
+      {
+        coo: { X: 0, Y: 0 },
+        data: [
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+          { name: 0, distance: 314.47481604473296, coordinates: { X: 1.0, Y: 1.0 } },
+        ],
+      },
+    ));
   });
 });
